11716e1e6766e85edc919fa1ab6ddf50768d6bda
Andrew Binnie <binnieandrew@gmail.com>
2020-02-03 15:41:16 +0000

    adding the developer test

---
+++ src/test/java/DeveloperTest.java
@@ -0,0 +1,41 @@
+import Staff.TechStaff.Developer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DeveloperTest {
+
+    Developer developer;
+
+    @Before
+    public void before(){
+        developer = new Developer("Kate", "OP3400", 120000.00);
+
+    }
+
+    @Test
+    public void canGetName() {
+        assertEquals("Kate", developer.getName());
+    }
+
+    @Test
+    public void canGetNINumber() {
+        assertEquals("OP3400", developer.getNINumber());
+    }
+
+    @Test
+    public void canGetSalary() {
+        assertEquals(120000, developer.getSalary());
+    }
+
+    @Test
+    public void canPayBonus() {
+        assertEquals("Kate", developer.getName());
+    }
+
+    @Test
+    public void canRaiseSalary() {
+        assertEquals(1200, developer.payBonus());
+    }
+}
